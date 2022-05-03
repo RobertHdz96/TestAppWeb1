d1434b7753a4851a42313336caaa165191f197d8
RobertHdz96 <53913978+RobertHdz96@users.noreply.github.com>
2022-05-03 13:15:38 -0500

    Initial commit

---
+++ TestAppWeb/Controller/loginController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TestAppWeb
+{
+    public class loginController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public void Get(string user, string pw)
+        {
+            var url = $"http://localhost:8080/apitest/getuser";
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            request.Accept = "application/json";
+            try
+            {
+                using (WebResponse response = request.GetResponse())
+                {
+                    using (Stream strReader = response.GetResponseStream())
+                    {
+                        if (strReader == null) return;
+                        using (StreamReader objReader = new StreamReader(strReader))
+                        {
+                        }
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                
+            }
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TestAppWeb/loginJS.js
@@ -0,0 +1,18 @@
+﻿function login()
+{
+    var user = document.getElementById("user").value;
+    var pw = document.getElementById("pw").value;
+
+        $.ajax({
+            type: "GET",
+            url: "/Controller/loginController",
+            data: { user: user, pw: pw},
+            success: function () {
+                    alert("success");
+            },
+            error: function () {
+                    alert("Some thing wrong..");
+                }
+        });
+}
+
